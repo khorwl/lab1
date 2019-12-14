163c97dd36287a53b3053ac80072a281449377e3
khorwl <khorwl@yandex.ru>
2019-12-14 08:16:50 +0500

    wip

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,19 +1,30 @@
-#include "button.h"
 #include <Arduino.h>
 #include <MD_TCS230.h>
 
-#define PIN_BUTTON_OFF 5
+#include "button.h"
+
 #define  S0_OUT  2
 #define  S1_OUT  3
 #define  S2_OUT  4
 #define  S3_OUT  5
 
-Button buttonOff(PIN_BUTTON_OFF);
 #define R_OUT 6
 #define G_OUT 7
 #define B_OUT 8
 
+#define R_CONTROL_PIN 9
+#define G_CONTROL_PIN 10
+#define B_CONTROL_PIN 11
+
 MD_TCS230 colorSensor(S2_OUT, S3_OUT, S0_OUT, S1_OUT);
+
+bool rButtonActivated = false;
+bool gButtonActivated = false;
+bool bButtonActivated = false;
+
+Button rButton(R_CONTROL_PIN);
+Button gButton(G_CONTROL_PIN);
+Button bButton(B_CONTROL_PIN);
 
 void setup()
 {
@@ -46,7 +57,22 @@ void loop()
 
     while (!colorSensor.available());
 
-    colorSensor.getRGB(&rgb);
+    if (rButton.wasPressed()){
+      Serial.println("Red button pressed");
+      rButtonActivated = !rButtonActivated;
+    }
+
+    if (gButton.wasPressed()) {
+      Serial.println("Green button pressed");
+      gButtonActivated = !gButtonActivated;
+    }
+
+    if (bButton.wasPressed()){
+      Serial.println("Blue button pressed");
+      bButtonActivated = !bButtonActivated;
+    }
+    
+    colorSensor.getRGB(&rgb); 
     print_rgb(rgb);
     set_rgb_led(rgb);
 }
@@ -60,10 +86,11 @@ void print_rgb(colorData rgb)
   Serial.print(rgb.value[TCS230_RGB_B]);
   Serial.println();
 }
-  
+
+
 void set_rgb_led(colorData rgb)
 {
-    analogWrite(R_OUT, 255 - rgb.value[TCS230_RGB_R]);
-    analogWrite(G_OUT, 255 - rgb.value[TCS230_RGB_G]);
-    analogWrite(B_OUT, 255 - rgb.value[TCS230_RGB_B]);
+    analogWrite(R_OUT, 255 - !rButtonActivated * rgb.value[TCS230_RGB_R]);
+    analogWrite(G_OUT, 255 - !gButtonActivated * rgb.value[TCS230_RGB_G]);
+    analogWrite(B_OUT, 255 - !bButtonActivated * rgb.value[TCS230_RGB_B]);
 }
